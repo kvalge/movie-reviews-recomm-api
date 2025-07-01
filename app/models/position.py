ec89358a16fbd18448b77b48de3b310ec31e02a8
Katrin Valge <valgekatrin@gmail.com>
2025-07-01 23:53:38 +0300

    Added back-end cast and crew positions tabel creation and CRUD operations.

---
+++ app/models/position.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Text
+from app.db.session import Base
+
+class Position(Base):
+    __tablename__ = "positions"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(256), unique=True, nullable=False)
+    description = Column(Text, nullable=True)
